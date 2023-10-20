eefcbe4dd274a22fd4304651056ee11ab187bdf0
bartekluc <145121953+bartekluc@users.noreply.github.com>
2023-10-20 12:40:56 +0200

    Add files via upload

---
+++ 19.10/about-us.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bartłomiej Łuć 4C</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+   <?php
+        require './partials/nav.php';
+
+        require './partials/footer.php';
+
+        include './partials/functions.php';
+        echo naglowek("O nas", 1)
+    ?>
+       <main>
+        <h1>
+        <label>O nas</label>
+
+
+         </h1>
+         <pre>
+         -Kto ty jesteś?
+        -Pijak mały.
+        -Jaki znak twój?
+        -Trzy browary
+        -Gdzie ty mieszkasz?
+        -Pod ławeczką.
+        -Czym się bronisz?
+        -Buteleczką !
+    </main> 
+ 
+</body>
+</html>
